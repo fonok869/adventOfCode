1a92c937315f06fde39fcfd3814bb72550d99730
Ferenc Molnar <molnar.ferenc.pro@tutanota.com>
2022-12-10 07:05:06 +0100

    [AoC] Solution Day 10 - 2022

--- src/main/java/com/fmolnar/code/year2022/Main2022.java
+++ src/main/java/com/fmolnar/code/year2022/Main2022.java
@@ -1,6 +1,6 @@
 package com.fmolnar.code.year2022;
 
-import com.fmolnar.code.year2022.day09.Day09Challenge02;
+import com.fmolnar.code.year2022.day10.Day10;
 
 import java.io.IOException;
 
@@ -8,7 +8,7 @@ public class Main2022 {
 
     public static void main(String[] args) throws IOException {
 
-        Day09Challenge02 day09 = new Day09Challenge02();
-        day09.calculate();
+        Day10 day10 = new Day10();
+        day10.calculate();
     }
 }
---
+++ src/main/java/com/fmolnar/code/year2022/day10/Day10.java
@@ -0,0 +1,59 @@
+package com.fmolnar.code.year2022.day10;
+
+import com.fmolnar.code.FileReaderUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day10 {
+
+    public void calculate() throws IOException {
+        List<String> lines = FileReaderUtils.readFile("/2022/day10/input.txt");
+
+        int cycle = 1;
+        int point = 1;
+        int positionCRT = 0;
+        List<String> pixels = new ArrayList<>();
+        List<Integer> sums = new ArrayList<>();
+        for (String line : lines) {
+            String[] splitted = line.split(" ");
+            sumUp(cycle, point, sums);
+            if (splitted.length == 1) {
+                calculCRT(positionCRT++, point, pixels);
+                cycle++;
+            } else {
+                calculCRT(positionCRT++, point, pixels);
+                cycle++;
+                calculCRT(positionCRT++, point, pixels);
+                sumUp(cycle++, point, sums);
+                point += Integer.valueOf(splitted[1]);
+            }
+        }
+
+        System.out.println("First: " + sums.stream().mapToInt(s->s).sum());
+
+        System.out.println("Second: ");
+        for (int i = 0; i < pixels.size(); i++) {
+            if (i % 40 == 0) {
+                System.out.println();
+            }
+            System.out.print(pixels.get(i));
+
+        }
+    }
+
+    private void sumUp(int cycle, int point, List<Integer> sums) {
+        if((cycle%40) == 20){
+            sums.add(cycle*point);
+        }
+    }
+
+    private void calculCRT(int cycle, int point, List<String> integs) {
+        if (Math.abs(point - (cycle % 40)) < 2) {
+            integs.add("#");
+        } else {
+            integs.add(".");
+        }
+    }
+}
