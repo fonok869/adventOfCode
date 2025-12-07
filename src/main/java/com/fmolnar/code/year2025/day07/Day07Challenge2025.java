b41be71ddf5ff6d8fa6cb945700c217fa799e3cc
Ferenc Molnar <molnarferi90@gmail.com>
2025-12-07 17:58:44 +0100

    [AoC] Day 7 Solution refactoring

--- src/main/java/com/fmolnar/code/year2025/day07/Day07Challenge2025.java
+++ src/main/java/com/fmolnar/code/year2025/day07/Day07Challenge2025.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day07Challenge2025 {
 
@@ -17,40 +18,58 @@ public class Day07Challenge2025 {
         Map<Point, Long> starter = new HashMap<>();
         Set<Point> splitter = new HashSet<>();
 
+        readInputAndGetStartAndSplitterPosition(lines, starter, splitter);
+
+        AtomicInteger counterSplitter = new AtomicInteger(0);
+        while (true) {
+            Map<Point, Long> newStarter = new HashMap<>();
+            starter.forEach((start, value) -> {
+                if (shouldSplitBeam(splitter, start)) {
+                    start.splitted().forEach(splittedPoint ->
+                            newStarter.compute(splittedPoint, (k1, v1) -> v1 == null ? value : v1 + value)
+                    );
+                    counterSplitter.incrementAndGet();
+                } else {
+                    newStarter.compute(start.downward(), (k1, v1) -> v1 == null ? value : v1 + value);
+                }
+            });
+            starter = new HashMap<>(newStarter);
+            if (isFinished(starter, lines)) {
+                break;
+            }
+        }
+        System.out.println("First: " + counterSplitter.get());
+        System.out.println("Second: " + starter.entrySet().stream().mapToLong(entry -> entry.getValue()).sum());
+
+    }
+
+    private static void readInputAndGetStartAndSplitterPosition(List<String> lines, Map<Point, Long> starter, Set<Point> splitter) {
         for (int y = 0; y < lines.size(); y++) {
             String line = lines.get(y);
             for (int x = 0; x < line.length(); x++) {
-                if ("S".equals(line.substring(x, x + 1))) {
+                if (isStarterPoint(line, x)) {
                     starter.put(new Point(y, x), 1L);
-                } else if ("^".equals(line.substring(x, x + 1))) {
+                } else if (isSplitterPoint(line, x)) {
                     splitter.add(new Point(y, x));
                 }
             }
         }
+    }
 
-        int counterSplitter = 0;
-        while (true) {
-            Map<Point, Long> newStarter = new HashMap();
-            for (Point start : starter.keySet()) {
-                Long value = starter.get(start);
-                if (splitter.contains(start.downward())) {
-                    Set<Point> splitted = start.splitted();
-                    for (Point splittedPoint : splitted) {
-                        newStarter.compute(splittedPoint, (k1, v1) -> v1 == null ? value : v1 + value);
-                    }
-                    counterSplitter++;
-                } else {
-                    newStarter.compute(start.downward(), (k1, v1) -> v1 == null ? value : v1 + value);
-                }
-            }
-            starter = new HashMap<>(newStarter);
-            if (starter.keySet().stream().mapToInt(s -> s.y()).filter(y -> lines.size() < y).findAny().isPresent()) {
-                break;
-            }
-        }
-        System.out.println(counterSplitter);
-        System.out.println("Second: " + starter.entrySet().stream().mapToLong(entry -> entry.getValue()).sum());
+    private static boolean isSplitterPoint(String line, int x) {
+        return "^".equals(line.substring(x, x + 1));
+    }
 
+    private static boolean isStarterPoint(String line, int x) {
+        return "S".equals(line.substring(x, x + 1));
+    }
+
+    private static boolean isFinished(Map<Point, Long> starter, List<String> lines) {
+        return starter.keySet().stream().mapToInt(s -> s.y()).filter(y -> lines.size() < y).findAny().isPresent();
+    }
+
+    private static boolean shouldSplitBeam(Set<Point> splitter, Point start) {
+        return splitter.contains(start.downward());
     }
 }
 
